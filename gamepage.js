7d45824c5eedb571fe6d4e7414b70aee65c8f9f2
Triptighatak <100752121+Triptighatak@users.noreply.github.com>
2023-10-24 17:42:41 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,37 @@
+// generation of random value
+var y= Math.floor(Math.random()*10+1);
+var guess=1;
+var playername=localStorage.getItem("player_name");
+
+function submit(){
+    var x= document.getElementById("guessField").value;
+
+    if(x==y)
+    {
+        alert("CONGRATULATIONS!!!!"+playername+" YOU GUESSED IT IN"+guess+"GUESS");
+        guess=1;
+
+
+    }
+
+    else if(x>y)
+    {
+        guess++;
+        alert("OOPS SORRY!!!! TRY A SMALLER NUMBER");
+    }
+    else
+    {
+        guess++;
+        alert("OOPS SORRY!!!! TRY A GREATER NUMBER");
+    }
+}
+
+function playAgain(){
+    y=math.floor(Math.random()*10+1);
+}
+
+
+// count of attempts
+// until hit
+  
+// function for the number sent by the user
